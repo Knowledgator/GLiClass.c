13f38fee8b5661df25b4f6d3dd6cb742b29e4eb7
MVYaroshenko <mykytayaroshenko@gmail.com>
2025-03-10 20:34:22 +0200

    fix: issue with interface types missalignment

--- src/model.c
+++ src/model.c
@@ -206,6 +206,23 @@ OrtValue* run_inference(OrtSession* session, OrtValue* input_ids_tensor, OrtValu
     return output_tensor;
 }
 
+wchar_t* convert_path(const char* path) {
+    size_t len = mbstowcs(NULL, path, 0);
+    if(len == (size_t)-1) {
+        fprintf(stderr, "Error: Unable to convert path to wchar_t*: %s\n", path);
+        exit(1);
+    }
+
+    wchar_t *wide_str = malloc((len + 1) * sizeof(wchar_t));
+    if(!wide_str) {
+        fprintf(stderr, "Error: Unable to convert path to wchar_t*: %s\n", path);
+        exit(1);
+    }
+
+    mbstowcs(wide_str, path, len + 1);
+    return wide_str;
+}
+
 /**
  * Creates and initializes an ONNX Runtime session from a model file.
  * 
@@ -263,10 +280,15 @@ OrtSession* create_ort_session(OrtEnv* env, const char* model_path, int num_thre
     #else
     printf("\tUsing CPU Execution Provider.\n");
     #endif
-    
 
     // Load the model and create a session
+    #ifdef _WIN32
+    wchar_t* path = convert_path(model_path); 
+    status = g_ort->CreateSession(env, path, session_options, &session);
+    free(path);
+    #else
     status = g_ort->CreateSession(env, model_path, session_options, &session);
+    #endif
     if (status != NULL) {
         const char* msg = g_ort->GetErrorMessage(status);
         fprintf(stderr, "Error: Failed to create session: %s\n", msg);
