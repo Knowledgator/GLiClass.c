fa9c096a9890a933bb70855016304b345736e157
MVYaroshenko <mykytayaroshenko@gmail.com>
2025-03-13 17:09:29 +0200

    chor: remove config headers

--- include/configs.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONFIGS_H
-#define CONFIGS_H
-
-#define BATCH_SIZE 8    // Number of texts in one batch for processing by the model
-#define MAX_LENGTH 2048 // Maximum length of tokenized text (number of tokens)
-#define THRESHOLD 0.5f  // Threshold for making a classification decision 
-#define NUM_THREADS 8   // Number of threads for CPU (does not affect GPU performance)
-
-#endif // CONFIGS_H
